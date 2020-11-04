2c3ce873037011b2820f617466420c101cdac626
hjcian <chutarojian@gmail.com>
2020-11-04 22:29:36 +0800

    ✨ Feat: Add basic flow to ShortenURL component

--- src/Components/ShortenURL.js
+++ src/Components/ShortenURL.js
@@ -1,9 +1,56 @@
 import React from 'react'
 import { Form, Input, Button, Message } from 'semantic-ui-react'
 
+// for demo, not for production
+const BACKEND_SERVER = 'http://127.0.0.1:12345'
+const BACKEND_SHORENURL_API = `${BACKEND_SERVER}/shortenURL`
+
+const constructShortUrl = (token) => {
+  return `${BACKEND_SERVER}/${token}`
+}
+
 const getTokenFromRemote = async (url) => {
-  const token = await 'asdfg'
-  return token
+  try {
+    const resp = await fetch(
+      `${BACKEND_SHORENURL_API}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          url
+        })
+      }
+    )
+    const { status } = resp
+    if (status === 200) {
+      const respj = await resp.json()
+      const { token } = respj
+      return [token, status]
+    } else {
+      return ['', status]
+    }
+  } catch (e) {
+    return ['', -1]
+  }
+}
+
+const OkMessage = ({ shortUrl }) => {
+  return (
+    <Message
+      info
+      content={`Shortened URL: ${shortUrl}`}
+    />
+  )
+}
+
+const ErrMessage = ({ status }) => {
+  return (
+    <Message
+      error
+      content={`Error! ... status code: ${status}`}
+    />
+  )
 }
 
 class ShortenURL extends React.Component {
@@ -11,18 +58,22 @@ class ShortenURL extends React.Component {
     super(props)
     this.state = {
       url: '',
-      token: ''
+      shortUrl: '',
+      isReqOk: true,
+      status: null
     }
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   async handleSubmit (event) {
-    console.log(`handleSubmit (url): ${this.state.url}`)
-    const token = await getTokenFromRemote(this.state.url)
-    console.log(`handleSubmit (token): ${token}`)
-    if (token !== '') {
-      this.setState({ token })
+    // should check if valid
+    const [token, status] = await getTokenFromRemote(this.state.url)
+    if (status === 200) {
+      const shortUrl = constructShortUrl(token)
+      this.setState({ shortUrl, status, isReqOk: true })
+    } else {
+      this.setState({ status, isReqOk: false })
     }
   }
 
@@ -33,7 +84,8 @@ class ShortenURL extends React.Component {
   }
 
   render () {
-    const { url, token } = this.state
+    const { url, shortUrl } = this.state
+    const { isReqOk, status } = this.state
     return (
       <>
         <Form onSubmit={this.handleSubmit}>
@@ -47,9 +99,11 @@ class ShortenURL extends React.Component {
             onClick={this.handleSubmit}
           />
         </Form>
-        <Message
-          content={`Shortened URL: ${token}`}
-        />
+        {
+          isReqOk
+            ? <OkMessage shortUrl={shortUrl} />
+            : <ErrMessage status={status} />
+        }
       </>
     )
   }
